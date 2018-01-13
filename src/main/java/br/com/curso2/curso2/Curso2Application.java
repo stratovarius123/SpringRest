0ea595488a80af213ca5ef855278ebb1bf87e0f5
Alexandre Reis <alexandre.jj.reis@gmail.com>
2018-01-13 12:36:12 -0200

    Primeiro Commit

---
+++ src/main/java/br/com/curso2/curso2/Curso2Application.java
@@ -0,0 +1,12 @@
+package br.com.curso2.curso2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curso2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Curso2Application.class, args);
+	}
+}
