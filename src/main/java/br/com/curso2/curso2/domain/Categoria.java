413ca2957a470d2c6f77d5099cef49a5e2a458d7
Alexandre Reis <alexandre.jj.reis@gmail.com>
2018-01-13 22:54:48 -0200

    Banco de Dados para Teste H2

--- src/main/java/br/com/curso2/curso2/domain/Categoria.java
+++ src/main/java/br/com/curso2/curso2/domain/Categoria.java
@@ -1,8 +1,21 @@
 package br.com.curso2.curso2.domain;
 
-public class Categoria {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categoria implements Serializable{
 	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
 	private Integer id;
+	
 	private String categoria;
 	
 	public Categoria(){
